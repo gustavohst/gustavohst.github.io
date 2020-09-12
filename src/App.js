6631717ead0bea8e630391c78c0ad55b90f83c21
Gustavo Tenório <gustavohst@msn.com>
2020-09-12 12:16:20 -0300

    First commit!

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <body>
+        <div className="mainContainter">
+        <div className="leftPanel">
+          LEFT
+        </div>
+        <div className="rightPanel">
+          RIGHT
+        </div>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
