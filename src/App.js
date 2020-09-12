8bd80ea1b850335403daa866cb6330c0106c8dae
Gustavo Tenório <gustavohst@msn.com>
2020-09-12 18:40:23 -0300

    create the portifolio template.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,45 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const skills = (
+  `const devSkills = [
+    'React JS',
+    'Javascript',
+    'HTML',
+    'CSS',
+    'SQL Server',
+    'C#',
+  ];
+          
+  const agile = [
+    'scrum',
+    'kanban'
+  ];`
+  );
+
   return (
-    <div>
-      <body>
-        <div className="mainContainter">
-        <div className="leftPanel">
-          LEFT
+    <div className="mainContainter">
+      <div className="leftPanel">
+        <pre className="preClass"> {skills} </pre>
+      </div>
+      <div className="rightPanel">
+        <div />
+        <div className="presentMe">
+          <div className="commonText">Hi I'm</div>
+          <div className="name">Gustavo Tenório</div>
+          <span className="bigPipe" />
+          <div className="commonText">FULL STACK DEVELOPER</div>
+          <div className="button">
+            <span>View CV</span>
+          </div>
         </div>
-        <div className="rightPanel">
-          RIGHT
+        <div className="socialMediaContainer">
+          <div className="mediumIconSquare linkedinIcon">in</div>
+          <div className="mediumIconSquare githubIcon">git</div>
+          <div className="mediumIconSquare devIcon">DEV</div>
         </div>
-        </div>
-      </body>
+      </div>
     </div>
   );
 }
