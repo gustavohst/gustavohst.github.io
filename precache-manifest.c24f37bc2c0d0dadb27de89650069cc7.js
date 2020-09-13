d1f2f9624f75419550ffb85ec3174a16f061fa59
Gustavo Tenório <gustavohst@msn.com>
2020-09-13 11:05:16 -0300

    Updates

---
+++ precache-manifest.c24f37bc2c0d0dadb27de89650069cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f989c7e9dcb85bec4261ff1a82d52a",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "ca1f5865c642cebf2d88",
+    "url": "/mywebsite/static/css/main.a9d8356f.chunk.css"
+  },
+  {
+    "revision": "edf50a8209be0cb75d84",
+    "url": "/mywebsite/static/js/2.32b87945.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mywebsite/static/js/2.32b87945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1f5865c642cebf2d88",
+    "url": "/mywebsite/static/js/main.39de730a.chunk.js"
+  },
+  {
+    "revision": "8bfdc81bc68db2f39e17",
+    "url": "/mywebsite/static/js/runtime-main.2e72d8b5.js"
+  }
+]);
